5e08def1653d8a7479ff993f36ba0089b6cd8ce6
Doğukan Filiz <dogukanfiliz@hotmail.com>
2023-12-07 13:28:00 +0300

    Hafta 2 Commit
    Ürün ücretleri belirlendi. Sepete alma butonu eklendi ve görsellikte düzenlemeye gidildi.

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 // Basit bir ürün veri yapısı
 const products = {
     'Bilgisayar': [
-        { name: 'Laptop', price: 0, image: 'img/laptop.png' },
+        { name: 'Laptop', price: 21000, image: 'img/laptop.png' },
     ],
     'Sağlık': [
-        { name: 'Vitaminler', price: 0, image: 'img/vitaminler.png' },
+        { name: 'Vitaminler', price: 430, image: 'img/vitaminler.png' },
     ],
     'Kadın Giyim': [
-        { name: 'Elbise', price: 0, image: 'img/elbise.png' },
+        { name: 'Elbise', price: 900, image: 'img/elbise.png' },
     ],
     'Erkek Giyim': [
-        { name: 'Gömlek', price: 0, image: 'img/gomlek.png' },
+        { name: 'Gömlek', price: 700, image: 'img/gomlek.png' },
     ],
     'Bebek': [
-        { name: 'Bebek Bezi', price: 0, image: 'img/bebek-bezi.png' },
+        { name: 'Bebek Bezi', price: 240, image: 'img/bebek-bezi.png' },
     ],
     'Elektronik': [
-        { name: 'Kamera', price: 0, image: 'img/kamera.png' },
+        { name: 'Kamera', price: 160000, image: 'img/kamera.png' },
     ],
     'Telefonlar': [
-        { name: 'Akıllı Telefon', price: 0, image: 'img/akilli-telefon.png' },
+        { name: 'Akıllı Telefon', price: 54000, image: 'img/akilli-telefon.png' },
     ],
     'Parfüm': [
-        { name: 'Erkek Parfümü', price: 0, image: 'img/erkek-parfum.png' },
+        { name: 'Erkek Parfümü', price: 4350, image: 'img/erkek-parfum.png' },
     ],
     'Makyaj': [
-        { name: 'Ruj', price: 0, image: 'img/ruj.png' },
+        { name: 'Ruj', price: 350, image: 'img/ruj.jpeg' },
     ],
     'Saat': [
-        { name: 'Erkek Saati', price: 0, image: 'img/erkek-saat.png' },
+        { name: 'Erkek Saati', price: 5600, image: 'img/erkek-saat.png' },
     ],
 };
 
@@ -59,7 +59,21 @@ function listProducts(category) {
         console.log(`Ürünler (${category}): Bu kategoriye ait ürün bulunmamaktadır.`);
     }
 }
+function listAllProducts() {
+    const productContainer = document.getElementById("product-list");
 
+    // Ürün listesini temizle
+    productContainer.innerHTML = '';
+
+    // Tüm kategorilerden birer ürün listele
+    for (const category in products) {
+        const categoryProducts = products[category];
+        if (categoryProducts.length > 0) {
+            const firstProduct = categoryProducts[0];
+            appendProduct(category, firstProduct);
+        }
+    }
+}
 function appendProduct(category, product) {
     const productContainer = document.getElementById("product-list");
 
@@ -70,17 +84,30 @@ function appendProduct(category, product) {
     productImage.src = product.image;
     productItem.appendChild(productImage);
 
-    const productName = document.createElement("div");
-    productName.textContent = product.name;
-    productItem.appendChild(productName);
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
 
-    const productPrice = document.createElement("div");
+    const productName = document.createElement("h3");
+    productName.textContent = product.name;
+    productInfo.appendChild(productName);
+
+    const productPrice = document.createElement("h4");
     productPrice.textContent = product.price + " ₺";
-    productItem.appendChild(productPrice);
+    productInfo.appendChild(productPrice);
+
+    const addToCart = document.createElement("button");
+    addToCart.textContent = "Sepete Ekle";
+    addToCart.addEventListener("click", function() {
+        // Sepete ekleme işlemleri burada yapılabilir.
+        // Örneğin: addToCartFunction(product);
+        console.log("Sepete Eklendi: " + product.name);
+    });
+    productPrice.appendChild(addToCart);
+
+    productItem.appendChild(productInfo);
 
     productContainer.appendChild(productItem);
 }
-
 function changeCurrency() {
     const currencySelector = document.getElementById("currency");
     const selectedCurrency = currencySelector.value;
